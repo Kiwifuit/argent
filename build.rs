d83dbe2d25b26deaa9fb51283d941dd62baf8aa2
Kiwifuit <mahkiwi123@gmail.com>
2024-03-12 08:03:25 -0700

    Deprecated `appmanifest.xml`

--- build.rs
+++ build.rs
@@ -1,24 +1,32 @@
-#[cfg(windows)]
-use winres::WindowsResource;
 #[cfg(not(windows))]
 use std::env::consts::OS;
-
 #[cfg(windows)]
-const MANIFEST_FILE: &str = include_str!("./appmanifest.xml");
+use winres::WindowsResource;
 
 #[cfg(windows)]
 fn append_resource() {
     let mut res = WindowsResource::new();
 
-    res.set_manifest(MANIFEST_FILE);
+    res.set_manifest(
+        r#"
+    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+    </assembly>
+    "#,
+    );
 
-    println!("cargo:warning=The `appmanifest.xml` file is added onto the executable");
     res.compile().unwrap();
 }
 
 #[cfg(not(windows))]
 fn append_resource() {
-    println!("cargo:warning=This is not a windows system! (system is {}) The `appmanifest.xml` file will not be added onto the executable", OS);
+    println!("cargo:warning=This is not a windows system! (system is {}) The program may not work as expected", OS);
 }
 
 fn main() {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod win;
 fn main() {
     let info = win::get_information();
 
+    println!("HELLOOOOO");
     println!("The info of the current system is listed below:");
     dbg!(info);
 }
